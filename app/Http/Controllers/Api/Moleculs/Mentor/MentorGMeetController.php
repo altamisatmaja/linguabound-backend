8464d72f8812c8e7450e7fa27d2d8ec20f95f5cd
altamisatmaja <altamisfattah16@gmail.com>
2024-06-03 18:24:36 +0700

    feat: seeder

--- app/Http/Controllers/Api/Moleculs/Mentor/MentorGMeetController.php
+++ app/Http/Controllers/Api/Moleculs/Mentor/MentorGMeetController.php
@@ -37,7 +37,6 @@ class MentorGMeetController extends Controller
         $meet->mentor_id = $request->user()->id;
         $meet->save();
 
-
         return response()->json(['message' => 'Kelas berhasil dibuat']);
     }
 
@@ -49,66 +48,5 @@ class MentorGMeetController extends Controller
         $meet->save();
 
         return response()->json(['message' => 'Kelas berhasil dipublish']);
-    }
-
-
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        try {
-            //code...
-        } catch (\Throwable $th) {
-            //throw $th;
-        }
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
--- app/Http/Controllers/Api/Organism/GMeetController.php
+++ app/Http/Controllers/Api/Organism/GMeetController.php
@@ -40,62 +40,10 @@ class GMeetController extends Controller
         $meetMember->remaja_id = $remajaId;
         $meetMember->save();
 
-        return response()->json(['message' => 'Remaja berhasil bergabung dalam kelas', 'meet_id' => $meetId]);
-    }
-
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Remaja berhasil bergabung dalam kelas',
+            'data' => $meetId
+        ]);
     }
 }
